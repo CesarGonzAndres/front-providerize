de347c3ba126b336075d0c1d0de34166efb934ef
cecas.gonzalezguerra <cecas.gonzalezguerra@externos-cl.cencosud.com>
2022-02-27 21:48:46 -0300

    added mobile menu

--- src/components/common/header/Header.js
+++ src/components/common/header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import M from 'materialize-css'
+import M from 'materialize-css'
 import {
   Link
 } from 'react-router-dom'
@@ -21,10 +21,10 @@ export default class Header extends React.Component {
       hover: true,
       belowOrigin: true
     }
-    M.Dropdown.init(dropdowns, options)
+    M.Dropdown.init(dropdowns, options) */
 
     const mobile = document.querySelectorAll('nav')
-    M.Sidenav.init(mobile) */
+    M.Sidenav.init(mobile)
   }
 
   render () {
@@ -39,6 +39,7 @@ export default class Header extends React.Component {
           </ul>
           <div className="nav-wrapper teal">
             <a href="#" className="brand-logo">Logo</a>
+            <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
               <li><Link to="/">Home</Link></li>
               <li><Link to="/contacts">Contacts</Link></li>
@@ -55,6 +56,13 @@ export default class Header extends React.Component {
             </ul>
           </div>
         </nav>
+        <ul className="sidenav" id="mobile-demo">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/contacts">Contacts</Link></li>
+          <li><Link to="/pricing">Pricing</Link></li>
+          <li><Link to="/about-us">About us</Link></li>
+          <li><Link to="/services">Services</Link></li>
+        </ul>
       </>
     )
   }
